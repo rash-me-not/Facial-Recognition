8b3d0fb09545d1c6e858b095f979e8cdd6a7375b
rashmita18 <rashmita.mishra18@gmail.com>
2020-05-13 22:29:11 -0700

    Removed the gaussian blur from driver since it was not used anywhere

--- driver.py
+++ driver.py
@@ -31,9 +31,9 @@ class Main:
         if not read_from_pickle:
             df = pd.read_csv(file, header=0)
 
-            train_samples = df[df.Usage == "Training"][:300]
-            val_samples = df[df.Usage == "PrivateTest"][:50]
-            test_samples = df[df.Usage == "PublicTest"][:25]
+            train_samples = df[df.Usage == "Training"]
+            val_samples = df[df.Usage == "PrivateTest"]
+            test_samples = df[df.Usage == "PublicTest"]
 
             samples_dict = {"train": train_samples, "val": val_samples, "test": test_samples}
             for type, samples in samples_dict.items():
@@ -88,48 +88,6 @@ class Main:
             x_feat.append(vector)
         return np.asarray(x), np.asarray(x_feat), np.asarray(y)
 
-    def gaussian_kernel(self, size, sigma=1, verbose=False):
-        kernel_1D = np.linspace(-(size // 2), size // 2, size)
-        for i in range(size):
-            kernel_1D[i] = self.dnorm(kernel_1D[i], 0, sigma)
-        kernel_2D = np.outer(kernel_1D.T, kernel_1D.T)
-        kernel_2D *= 1.0 / kernel_2D.max()
-        if verbose:
-            plt.imshow(kernel_2D, interpolation='none', cmap='gray')
-            plt.title("Image")
-            plt.show()
-        return kernel_2D
-
-    def dnorm(self, x, mu, sd):
-        return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
-
-    def convolution(self, image, kernel, average=False, verbose=False):
-        image_row, image_col = image.shape
-        kernel_row, kernel_col = kernel.shape
-        output = np.zeros(image.shape)
-        pad_height = int((kernel_row - 1) / 2)
-        pad_width = int((kernel_col - 1) / 2)
-        padded_image = np.zeros((image_row + (2 * pad_height), image_col + (2 * pad_width)))
-        padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
-        for row in range(image_row):
-            for col in range(image_col):
-                output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
-        if average:
-            output[row, col] /= kernel.shape[0] * kernel.shape[1]
-
-        print("Output Image size : {}".format(output.shape))
-
-        if verbose:
-            plt.imshow(output, cmap='gray')
-            plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
-            plt.show()
-
-        return output
-
-    def gaussian_blur(self, image, kernel_size, verbose=False):
-        kernel = self.gaussian_kernel(kernel_size, sigma=math.sqrt(kernel_size), verbose=verbose)
-        return self.convolution(image, kernel, average=True, verbose=verbose)
-
     def plot_confusion_matrix(self, cm, classes,
                               normalize=False,
                               title='Confusion matrix',
@@ -164,6 +122,8 @@ class Main:
 if __name__ == "__main__":
     file = "../../fer2013/fer2013.csv"
     main = Main(file)
+
+    print("Generating Dataset")
     x_train, x_feat_train, y_train, \
     x_val, x_feat_val, y_val, x_test, x_feat_test, y_test = main.generate_dataset(read_from_pickle=False,
                                                                                   augment_data=True)
